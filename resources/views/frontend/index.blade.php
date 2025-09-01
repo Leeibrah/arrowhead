8ac03b4a674be2bd553a7a28e59a4c1f876c7a16
Lee Ibrahim <leeibrah@gmail.com>
2025-09-01 22:38:41 +0300

    Replace landing video

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -222,7 +222,7 @@
 
             <div class="video-container">
               <iframe
-                src="https://www.youtube.com/embed/NuBUbJvx6ac"
+                src="https://www.youtube.com/embed/7cauB4VMMbM"
                 title="YouTube video"
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
